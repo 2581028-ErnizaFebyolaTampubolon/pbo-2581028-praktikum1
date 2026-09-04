bcf6a05225ac66b40ed6cd3024f8b22879c1adbd
Erniza Febyola Tampubolon <2581028@unai.edu>
2026-09-04 13:34:19 +0700

    menambahkan prodi dan asal

--- src/Biodata.java
+++ src/Biodata.java
@@ -2,6 +2,8 @@ public class Biodata {
     public static void main(String[] args) {
         String nama = "Erniza Febyola Tampubolon";
         String nim = "2581028";
+        String prodi = "Teknik Informatika";
+        String asal = "Karawang";
         System.out.println("Nama : " + nama);
         System.out.println("NIM  : " + nim);
     }
