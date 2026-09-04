e3d02d12ac5f29989e20246b2b3bbf25ece9dc15
Erniza Febyola Tampubolon <2581028@unai.edu>
2026-09-04 13:23:35 +0700

    mengumpulkan pratikum1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Biodata {
+    public static void main(String[] args) {
+        String nama = "Erniza Febyola Tampubolon";
+        String nim = "2581028";
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM  : " + nim);
+    }
+}
